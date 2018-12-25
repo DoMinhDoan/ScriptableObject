055b4a1c9717c622ab1d6a9ff29f9575186fb2f6
dodoan <dodoan.it@gmail.com>
2018-12-25 23:31:01 +0700

    + UI canvas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-   public void UpdateDisplayUI(AnimalData animalData)
+    public Text animalName;
+    public Text animalSound;
+    public Text animalAge;
+    public Text animalCost;
+    public Image animalIcon;
+
+    public void UpdateDisplayUI(AnimalData animalData)
     {
-        Debug.Log("UpdateDisplayUI Animal=" + animalData.animalName);
+        animalName.text = animalData.animalName;
+        animalSound.text = animalData.animalSound;
+        animalAge.text = animalData.animalAge.ToString();
+        animalCost.text = animalData.animalGoldCost.ToString();
+        animalIcon.sprite = animalData.animalIcon;
     }
 }
