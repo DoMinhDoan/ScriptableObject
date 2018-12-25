877fb07bf8d40bf178165baa55728504fcf50987
dodoan <dodoan.it@gmail.com>
2018-12-25 22:51:41 +0700

    - creating scriptable objects

---
+++ Assets/Scripts/AnimalController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalController : MonoBehaviour
+{
+    public AnimalData animal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Animal : " + animal.animalName);
+    }
+}
---
+++ Assets/Scripts/AnimalData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Animal.asset", menuName = "Animal", order = 51)]
+public class AnimalData : ScriptableObject
+{
+    public string animalName;
+    public string animalSound;
+    public Sprite animalIcon;
+    public int animalGoldCost;
+    public int animalAge;
+}
